a561dfe95b290c0ced548ebef6b8f4c948dca314
Optem Sokolov <optemsokolov@yandex.ru>
2023-01-25 20:28:02 +0300

    Разработал JS

---
+++ script.js
@@ -0,0 +1,32 @@
+let container = document.querySelector('.container');
+const editButton = container.querySelector('.profile__edit-button');
+const addButton = container.querySelector('.profile__add-button');
+const likeButton = container.querySelector('.card__like-button');
+const closeButton = container.querySelector('.popup__close-icon');
+const saveButton = container.querySelector('.popup__container');
+let popup = container.querySelector('.popup');
+let inputUsername = container.querySelector('.popup__input_name');
+let inputAbout = container.querySelector('.popup__input_about');
+let username = container.querySelector('.profile__info_name');
+let about = container.querySelector('.profile__info_description');
+
+function editProfile() {
+  inputUsername.value = username.textContent;
+  inputAbout.value = about.textContent;
+  popup.classList.add('popup_opened');
+};
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+};
+
+function saveUserInfo(evt) {
+  evt.preventDefault();
+  username.textContent = inputUsername.value;
+  about.textContent = inputAbout.value;
+  closePopup();
+};
+
+editButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closePopup);
+saveButton.addEventListener('submit', saveUserInfo);

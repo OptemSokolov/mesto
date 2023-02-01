721f3b9c12a8433ff85c850503abb0880fff94b2
Optem Sokolov <optemsokolov@yandex.ru>
2023-02-01 23:31:19 +0300

    Фиксы после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 let container = document.querySelector('.container');
+let popup = document.querySelector('.popup');
 const editButton = container.querySelector('.profile__edit-button');
 const addButton = container.querySelector('.profile__add-button');
 const likeButton = container.querySelector('.card__like-button');
-const closeButton = container.querySelector('.popup__close-icon');
-const saveButton = container.querySelector('.popup__container');
-let popup = container.querySelector('.popup');
-let inputUsername = container.querySelector('.popup__input_name');
-let inputAbout = container.querySelector('.popup__input_about');
+const closeButton = popup.querySelector('.popup__close-icon');
+const saveButton = popup.querySelector('.popup__container');
+let inputUsername = popup.querySelector('.popup__input_value_name');
+let inputAbout = popup.querySelector('.popup__input_value_about');
 let username = container.querySelector('.profile__title');
 let about = container.querySelector('.profile__description');
 

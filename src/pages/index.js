127166ff1a47de6dab78322fc689cfb92b3ce05c
Optem Sokolov <optemsokolov@yandex.ru>
2023-04-07 22:06:36 +0300

    ПР8 закончил Вэбпак

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,12 @@
 import { Card } from "../components/Card.js";
 import { FormValidator } from "../components/FormValidator.js";
-import { Popup } from "../components/Popup.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { Section } from "../components/Section.js";
 import { UserInfo } from "../components/UserInfo.js";
 
+import './index.css';
 
-// // const popupProfile = document.querySelector('.popup-profile');
-// const inputUsername = document.querySelector('#username');
-// const inputAbout = document.querySelector('#about');
-// const username = document.querySelector('.profile__title');
-// const about = document.querySelector('.profile__description');
-// const popupPlace = document.querySelector('.popup-place');
-// // const popupImage = document.querySelector('.popup-image');
-// const inputPlaceName = document.querySelector('#place-name');
-// const inputPlaceLink = document.querySelector('#place-link');
 const placeForm = document.forms['form-place'];
 const profileForm = document.forms["form-profile"];
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -136,79 +127,3 @@ const editProfileForm = new FormValidator(formValidationList, profileForm);
 editProfileForm.enableValidation();
 const addCardForm = new FormValidator(formValidationList, placeForm);
 addCardForm.enableValidation();
-
-
-
-
-// -------------------------------------------------------------------
-
-// // Функция появления формы (общая)
-// const openPopup = (popup) => {
-//   popup.classList.add('popup_opened');
-//   popup.addEventListener("mousedown", closePopupOverlay);
-//   document.addEventListener("keydown", (closePopupEscape));
-// };
-// // Функция скрытия формы (общая)
-// const closePopup = (popup) => {
-//   popup.classList.remove('popup_opened');
-//   popup.removeEventListener("mousedown", closePopupOverlay);
-//   document.removeEventListener("keydown", (closePopupEscape));
-// };
-// // Функция закрытия попапов нажатием на крестик
-// const closeButtons = document.querySelectorAll('.popup__close-icon');
-// closeButtons.forEach((button) => {
-//   const popup = button.closest('.popup');
-//   button.addEventListener('click', () => closePopup(popup));
-// });
-// // Функция закрытия попапа по клику на ескейп
-// const closePopupEscape = (evt) => {
-//   if (evt.key === "Escape") {
-//     const openedPopup = document.querySelector(".popup_opened");
-//     closePopup(openedPopup);
-//   }
-// };
-// // Функция закрытия попапа по клику на оверлей
-// const closePopupOverlay = (event) => {
-//   if (event.target === event.currentTarget) {
-//     closePopup(event.currentTarget);
-//   }
-// };
-
-// // Функция срохранения изменений профиля
-// const saveUserInfo = (evt) => {
-//   evt.preventDefault();
-//   username.textContent = inputUsername.value;
-//   about.textContent = inputAbout.value;
-//   closePopup(popupProfile);
-// };
-
-// // Функция редактирования профиля
-// const editProfile = () => {
-//   inputUsername.value = username.textContent;
-//   inputAbout.value = about.textContent;
-//   openPopup(popupProfile);
-// };
-
-// // Функция открытия формы добавления плэйс
-// const openPopupPlace = () => {
-//   openPopup(popupPlace);
-// };
-
-// Функция сохранения новой карточки
-// const submitPopupPlace = (evt) => {
-//   evt.preventDefault();
-//   const newCard = {
-//     name: inputPlaceName.value,
-//     link: inputPlaceLink.value,
-//   };
-//   renderCard(newCard);
-//   closePopup(popupPlace);
-//   evt.target.reset();
-// };
-
-// // Вызов функций попап профиль
-// profileEditButton.addEventListener('click', editProfile);
-// profileForm.addEventListener('submit', saveUserInfo);
-// // Вызов функций попап плэйс
-// addButton.addEventListener('click', openPopupPlace);
-// placeForm.addEventListener('submit', submitPopupPlace);
